607a72984adff85e9e8ccf0b282d1e99d4548e47
Nerv3sine <jasoncai64@gmail.com>
2024-03-24 02:54:09 -0700

    polished website styling
    polished navbar
    polished the locations page, prepped for data formatting
    adjusted the main map page
    added placeholder image for the locations page

--- src/Pages/Locations.tsx
+++ src/Pages/Locations.tsx
@@ -3,14 +3,16 @@ import './Locations.css';
 import CardContainer from '../Components/CardContainer/CardContainer';
 import Card from '../Components/Card/Card';
 import NavMenu from '../Components/NavMenu';
-
+import placeHolderImg from '../Assets/placeholder.jpg';
 
 const Locations = () => {
     return (
         <div className='main'>
             <NavMenu />
-            <div className='banner'> 
-
+            <div className='banner'>
+                <img className='placeHolder' id="bannerImg" src={placeHolderImg}/>
+                <div id='bannerTint'></div>
+                <h1 id='galleryName'>Perspective Gallery</h1>
             </div>
             <div className='disp_img'>
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, useNavigate} from 'react-router-dom';
 import App from './App';
-
+import './App.css';
 
 
 
